ed744a01ed55e7fac48d26f6effe6f6ea4d2c9b2
Gauthamkumar15 <a.r.gauthamkumar@gmail.com>
2022-08-16 17:34:20 +0000

    second commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,11 @@ import Cookies from 'js-cookie'
 import Header from '../Header'
 
 class Home extends Component {
+  onFindJobs = () => {
+    const {history} = this.props
+    history.replace('/jobs')
+  }
+
   render() {
     const jwtToken = Cookies.get('jwt_token')
     if (jwtToken === undefined) {
@@ -14,12 +19,19 @@ class Home extends Component {
     return (
       <>
         <Header />
-        <div>
-          <h1>Find The Job That Fits Your Life</h1>
-          <p>
+        <div className="home-container">
+          <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+          <p className="home-para">
             Millions of people are searching for jobs, information, company
             reviews. Find the job that fits your abilities and potential.
           </p>
+          <button
+            onClick={this.onFindJobs}
+            type="button"
+            className="home-button"
+          >
+            Find Jobs
+          </button>
         </div>
       </>
     )
